7e4ed06865c39276fc15038ae7aae25b172ac6e6
aleifericsson <alifnafili@gmail.com>
2023-08-26 20:26:43 +0400

    quick updat

--- src/List.jsx
+++ src/List.jsx
@@ -1,46 +1,68 @@
 import Rea from "./assets/react.svg"
+import { useState } from "react";
+import {v4} from "uuid";
 //import PropTypes from 'prop-types';
 
-const sonicChars = ["Sonic", "Shadow", "Tangle", "Silver", "Amy", "Blaze", "Rouge", "Knuckles", "Tails", "Whisper"];
-const sonicCols = ["royalblue", "darkslategray", "lightslategray", "silver", "pink", "plum", "hotpink", "crimson", "orange", "sandybrown"];
 
-export default function List(){
+const List = () =>{
+    const [sonicChars, sChadd] = useState(["Sonic", "Shadow", "Tangle", "Silver", "Amy", "Blaze", "Rouge", "Knuckles", "Tails", "Whisper"]);
+    const [sonicCols, sCoadd] = useState(["royalblue", "darkslategray", "lightslategray", "silver", "pink", "plum", "hotpink", "crimson", "orange", "sandybrown"]);
+
+
     return(
         <div>
             <h2>Sonic Chars</h2>
             <ul>
                 {sonicChars.map((cha) => {
-                    return (<li key = {cha} className = "sonic-char" style = {myStyle(sonicChars.indexOf(cha))}>{cha}</li>);
+                    return (<li key = {cha} className = "sonic-char" style = {myStyle(sonicCols[sonicChars.indexOf(cha)])}>{cha}</li>);
                 })}
             </ul>
             <img src={Rea}></img>
+            <Button color="red" fontSize={40} text="please work" whenclick={()=>{ //ignore, doesn't work
+                const newChar = addMetalChar(sonicChars)
+                sChadd(newChar);
+                const newCol = addMetalCol(sonicCols);
+                sCoadd(newCol);
+                console.log("metal");
+                
+            }}/>
         </div>
     )
 }
 
-function myStyle(index){ //style uses regular js objects, no special jsx or whatever
+const addMetalChar = (char) =>{
+    let newChar = char;
+    newChar.push(v4());
+    return newChar;
+}
+
+const addMetalCol = (char) =>{
+    let newChar = char;
+    newChar.push("rgb(86, 101, 115)");
+    return newChar;
+}
+
+function myStyle(col){ //style uses regular js objects, no special jsx or whatever
     return {
         color: "white",
-        backgroundColor: sonicCols[index],
+        backgroundColor: col,
         border: `1px solid white`,
         padding: "2px"
     }
 }
 
-/* props just doesn't work it seems
 
-
-function Button({color, fontSize, text = "heehee"}) {
+function Button({color, fontSize, text = "heehee", whenclick}) {
     const buttonStyle = {
       color: color,
       fontSize: fontSize + 'px'
     };
   
     return (
-      <button style={buttonStyle}>{text}</button>
+      <button style={buttonStyle} onClick={whenclick}>{text}</button>
     )
   }
-
+/*
   
 Button.PropTypes = {
     color: PropTypes.string,
@@ -54,3 +76,5 @@ Button.PropTypes = {
             < Button text="Don't Click Me!" color="red" fontSize={12} />
             <Button text="Click Me!" color="blue" fontSize={20} />
 */
+
+export default List;
